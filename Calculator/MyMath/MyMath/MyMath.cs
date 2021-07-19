228d97ae77fa38399750090aee08f9dcfd892c19
Adan Asim <67644268+Adan-Asim@users.noreply.github.com>
2021-07-20 00:48:29 +0500

    Add files via upload

---
+++ Calculator/MyMath/MyMath/MyMath.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class MyamathFunctions
+    {
+        public static double Add(double a, double b)
+        {
+            return a+b;
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static long Add(params int []aray)
+        {
+            long sum = 0;
+            foreach(var x in aray)
+            {
+                sum += x;
+            }
+            return sum;
+        }
+
+        public static double Add(params Double [] aray)
+        {
+            double sum = 0.0;
+            foreach(var x in aray)
+            {
+                sum += x;
+            }
+            return sum;
+        }
+
+        public static int Sub(params int [] aray)
+        {
+            int sum = aray[0]*2;
+            foreach (var x in aray)
+                sum -= x;
+            return sum;
+        }
+
+        public static long Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static (int,int) Division(int a, int b)
+        {
+            int quotient = a / b;
+            int remainder = a % b;
+
+            return (quotient,remainder);
+        }
+    }
+}
