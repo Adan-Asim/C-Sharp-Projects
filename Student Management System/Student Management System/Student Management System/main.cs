a992dc231f4ba6f90135b9bdabc2b2dfb2c2c872
Adan Asim <67644268+Adan-Asim@users.noreply.github.com>
2021-07-20 01:15:35 +0500

    Add files via upload
    A basic Student Management System in which you can enter the record of students.
    
    A class Student has public properties Sid, Sname, Semail, Scgpa. 
    A Class University has Uid, University_name and list<student> list_of_students. The University class has a function ADDSTUDENT that takes student name, cgpa and email from the user using the console. (Student Id is auto-incremented with a static int Sid = 0. When an object is created then Sid is incremented and used). This function creates a student class object and sets an instance variable according to user input and adds this object into list_of_students. 
    
    In University Class a storeIntoDatabase function saves all the student's objects from list_of_students defined in a university class, into database table “dbo.student”.
    In University Class a GetFromDatabase function that will select all the students from database table “dbo.student” and display on the console in the format, My id is 10, my name is “danish” and my cgpa is 1.2 and you may contact me at ****@***.** 
       
    In university Class, there are also Update and Delete methods that take Sid as an argument and delete and update records from the database. 
    
    A Static void Main () function, creates an object of university class and adds some students to the database. View students from the database. And also checks the Delete and update functions as well.
    
    There is Proper Exception handling (TRY CATCH FINALLY Throw)
    There are Parameterized Queries to avoid SQL Injection.

---
+++ Student Management System/Student Management System/Student Management System/main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Student_Management_System
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                University pucit = new University(101, "PUCIT");
+
+                pucit.ADDSTUDENT();
+                pucit.ADDSTUDENT();
+                pucit.storeIntoDatabase();
+
+                pucit.Delete(14);
+                pucit.Update(15, "Abc", "aaa@gmail.com", 3.8);
+                pucit.GetFromDatabase();
+            }
+            catch
+            {
+                Console.WriteLine("ENTER CORRECT DATA TYPE COMPATIBLE VALUES");
+            }
+        }
+    }
+}
